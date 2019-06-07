a0f0169e16f8cf357d19a7b3347edcef4608008d
Jeffrey Neil Willits <48331906+jnwillits@users.noreply.github.com>
2019-06-06 18:57:23 -1000

    Update report-email-utility.py

--- report-email-utility.py
+++ report-email-utility.py
@@ -1,8 +1,8 @@
 # !/usr/bin/env python
-
 """
 Jeff's Report Email Utility - Version 1.0
-This is a utility that is executed daily by Windows Scheduler to send specific files to a distribution. It does nothing if the files do no exist. See my GitHub repository for the latest version.   Jeffrey Neil Willits     jnwillits.com
+This utility is executed daily by Windows Task Scheduler to send specific files to a distribution. It does
+nothing if the files are absent. See my GitHub repository for the latest version.   Jeff Willits  jnwillits.com
 """
 
 import win32com.client as win32
@@ -22,19 +22,20 @@ def get_paths(path):
     return attachment, attachment_exists
 
 
-def core_tasks(file, attachment, attachment_exists):
+def core_tasks(new_mail_pass, file, attachment, attachment_exists):
     if attachment_exists:
-        new_mail.Attachments.Add(Source=str(attachment))
+        new_mail_pass.Attachments.Add(Source=str(attachment))
         os.remove(file)
+    return new_mail_pass
 
 
 if __name__ == '__main__':
-
     file1 = 'filename1.pdf'
     file2 = 'filename2.pdf'
     file3 = 'filename3.pdf'
 
-    to_email = """ < email1@gmail.com >; < email2@gmail.com > """
+    to_email = """ email1@mac.com;
+                   email2@gmail.com;  """
 
     attachment1, attachment1_exists = get_paths(file1)
     attachment2, attachment2_exists = get_paths(file2)
@@ -45,9 +46,9 @@ if __name__ == '__main__':
     new_mail.Subject = f'Summary Files for {date.today():%m/%d/%y}'
     new_mail.To = to_email
 
-    core_tasks(file1, attachment1, attachment1_exists)
-    core_tasks(file2, attachment2, attachment2_exists)
-    core_tasks(file3, attachment3, attachment3_exists)
+    new_mail = core_tasks(new_mail, file1, attachment1, attachment1_exists)
+    new_mail = core_tasks(new_mail, file2, attachment2, attachment2_exists)
+    new_mail = core_tasks(new_mail, file3, attachment3, attachment3_exists)
 
     if attachment1_exists or attachment2_exists or attachment3_exists:
         new_mail.Send()
